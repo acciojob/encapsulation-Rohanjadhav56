3830cc8412e5832c6c1b5fea7cfdaab22fd26d69
Rohan Jadhav <rohanjadhav8174@gmail.com>
2023-08-28 16:13:53 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly rwOnly = new RWOnly();
+//      rwOnly.name = "Rohan";
+        //name variable is private
+
+        rwOnly.setName("Rohan");
+        System.out.println(rwOnly.getName());
+    }
 }
